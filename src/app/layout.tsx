8605a318f8f3bc9c3bd7ab8016cbaa7af22b88d2
Kyrylo <dexter37814@gmail.com>
2024-01-08 02:22:00 +0200

    test: try to add some lint configurations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'plug',
-  description: 'plug',
+    title: 'plug',
+    description: 'plug',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en'>
+            <body
+                className={cn(
+                    'min-h-screen bg-background font-sans antialiased',
+                    inter.className
+                )}
+            >
+                {children}
+                Hello World
+                <Button>CLick Me</Button>
+            </body>
+        </html>
+    )
 }
