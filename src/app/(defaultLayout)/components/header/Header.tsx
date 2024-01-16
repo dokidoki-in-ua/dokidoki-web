379a7c7516591aded384567e1413758dd42064e2
Kyrylo <dexter37814@gmail.com>
2024-01-16 16:51:36 +0200

    feat: add menu marking

--- src/app/(defaultLayout)/components/header/Header.tsx
+++ src/app/(defaultLayout)/components/header/Header.tsx
@@ -5,11 +5,19 @@ import { RiArrowDropDownFill } from 'react-icons/ri'
 const Header = () => {
     return (
         <div className='h-[60px] fixed w-screen flex justify-center z-10 bg-primary '>
-            <div className='w-10/12 flex justify-between px-[20px]'>
+            <div className='w-10/12 flex justify-between px-[20px] text-[16px]'>
                 <div className='flex h-full'>
-                    <Link href={'/'} className='px-[10px] fill-secondary hover:fill-white duration-200'>
+                    <Link
+                        href={'/'}
+                        className='px-[10px] fill-secondary hover:fill-white duration-200'
+                    >
                         <Logo className='w-[150px]' />
                     </Link>
+                    <div className='flex'>
+                        <div className='flex items-center px-2'>Аніме</div>
+                        <div className='flex items-center px-2'>Новини</div>
+                        <div className='flex items-center px-2'>Манга</div>
+                    </div>
                 </div>
             </div>
         </div>
