b0be9adc5748eac81e781177caa4e51dd01e3a4c
Kyrylo <dexter37814@gmail.com>
2024-01-16 15:46:56 +0200

    feat(styles): add logo styles

--- src/app/(defaultLayout)/components/header/Header.tsx
+++ src/app/(defaultLayout)/components/header/Header.tsx
@@ -1,9 +1,17 @@
+import { Logo } from '@/components/svg/Logo'
+import Link from 'next/link'
 import { RiArrowDropDownFill } from 'react-icons/ri'
 
 const Header = () => {
     return (
-        <div className='flex h-[60px] items-stretch fixed w-screen z-10 bg-primary'>
-
+        <div className='h-[60px] fixed w-screen flex justify-center z-10 bg-primary '>
+            <div className='w-10/12 flex justify-between px-[20px]'>
+                <div className='flex h-full'>
+                    <Link href={'/'} className='px-[10px] fill-secondary hover:fill-white duration-200'>
+                        <Logo className='w-[150px]' />
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
--- src/app/(defaultLayout)/layout.tsx
+++ src/app/(defaultLayout)/layout.tsx
@@ -1,7 +1,10 @@
+import Header from "./components/header/Header"
+
 const DefaultLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div className='flex flex-col h-full min-h-full'>
-
+            <Header/>
+            {children}
         </div>
     )
 }
