a01799df649b462e02493d1539ce1b3bc96e6ae3
Kyrylo <dexter37814@gmail.com>
2024-01-17 12:54:17 +0200

    test: font issue fix

--- src/app/(defaultLayout)/components/header/Header.tsx
+++ src/app/(defaultLayout)/components/header/Header.tsx
@@ -5,7 +5,7 @@ import { RiArrowDropDownFill } from 'react-icons/ri'
 const Header = () => {
     return (
         <div className='h-[60px] fixed w-screen flex justify-center z-10 bg-primary '>
-            <div className='w-10/12 flex justify-between px-[20px] text-[16px]'>
+            <div className='w-10/12 flex justify-between px-[20px]'>
                 <div className='flex h-full'>
                     <Link
                         href={'/'}
@@ -14,9 +14,9 @@ const Header = () => {
                         <Logo className='w-[150px]' />
                     </Link>
                     <div className='flex'>
-                        <div className='flex items-center px-2'>Аніме</div>
-                        <div className='flex items-center px-2'>Новини</div>
-                        <div className='flex items-center px-2'>Манга</div>
+                        <div className='flex items-center px-2'>Anime</div>
+                        <div className='flex items-center px-2'>News</div>
+                        {/* <div className='flex items-center px-2'>Манга</div> */}
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,9 @@ module.exports = {
             },
         },
         extend: {
+            fontFamily: {
+                sans: ['var(--font-rubik)', ...fontFamily.sans],
+            },
             colors: {
                 background: 'var(--background)',
                 font: {
@@ -42,28 +45,6 @@ module.exports = {
                 span: {
                     DEFAULT: 'var(--span)',
                 },
-            },
-            borderRadius: {
-                lg: 'var(--radius)',
-                md: 'calc(var(--radius) - 2px)',
-                sm: 'calc(var(--radius) - 4px)',
-            },
-            keyframes: {
-                'accordion-down': {
-                    from: { height: '0' },
-                    to: { height: 'var(--radix-accordion-content-height)' },
-                },
-                'accordion-up': {
-                    from: { height: 'var(--radix-accordion-content-height)' },
-                    to: { height: '0' },
-                },
-            },
-            animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
-            },
-            fontFamily: {
-                sans: ['var(--font-sans)', ...fontFamily.sans],
             },
         },
     },
