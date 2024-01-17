0972837b11da3ebcd3fc935ad927db1d9ad63cdc
Kyrylo <dexter37814@gmail.com>
2024-01-17 13:36:21 +0200

    fix: font issue and add styles for nemu marking

--- src/app/(defaultLayout)/components/header/Header.tsx
+++ src/app/(defaultLayout)/components/header/Header.tsx
@@ -9,14 +9,27 @@ const Header = () => {
                 <div className='flex h-full'>
                     <Link
                         href={'/'}
-                        className='px-[10px] fill-secondary hover:fill-white duration-200'
+                        className='px-[10px] fill-secondary hover:fill-white duration-200 flex items-center'
                     >
                         <Logo className='w-[150px]' />
                     </Link>
                     <div className='flex'>
-                        <div className='flex items-center px-2'>Anime</div>
-                        <div className='flex items-center px-2'>News</div>
-                        {/* <div className='flex items-center px-2'>Манга</div> */}
+                        <div className='flex items-center px-2 hover:bg-primary-hover hover:text-font-hover duration-200 cursor-pointer'>
+                            <span>Аніме</span>
+                            <RiArrowDropDownFill size={22} />
+                        </div>
+                        <Link
+                            href={''}
+                            className='flex items-center px-2 hover:bg-primary-hover hover:text-font-hover duration-200'
+                        >
+                            Новини
+                        </Link>
+                        <Link
+                            href={''}
+                            className='flex items-center px-2 hover:bg-primary-hover hover:text-font-hover duration-200'
+                        >
+                            Манга
+                        </Link>
                     </div>
                 </div>
             </div>
